3499811930d2b68a2988387862fd8774fbe4e8d6
Jon Schneider <jon@jonschneider.me>
2025-02-25 16:50:31 -0500

    Adds Example project that imports InterviewDataStructures Swift Package

---
+++ Example/Source/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftInterviewDataStructures
+//
+//  Created by Jon Schneider on 2/25/25.
+//
+
+import Foundation
+import InterviewDataStructures
+
+print("Hello, World!")
+
